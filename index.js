b9abd2d2488e4334d34e1003a4f0642200ccb1df
Cuneyt Ergun <cuneytergun@gmail.com>
2024-05-11 22:51:44 +0100

    Add rate limiting to API endpoints

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
@@ -7,6 +8,14 @@ const HOST = process.env.HOST || "localhost";
 
 // Create a new express application
 const app = express();
+
+// Rate limiter
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
+app.set("trust proxy", 1);
 
 // Enable CORS
 app.use(cors());
